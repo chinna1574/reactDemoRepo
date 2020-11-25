c00d586767aeb070d62ff137694cf50b2befa224
chinna1574 <75009432+chinna1574@users.noreply.github.com>
2020-11-25 16:28:44 +0530

    saved

--- src/components/Person/PersonCard.js
+++ src/components/Person/PersonCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Person.css';
 
 export default function PersonCard(props){
+    console.log('[PC] rendered')
 let design = (
 <div className='PersonStyle' onClick={()=> props.selectPerson(props.person.id)}> 
     Name:&nbsp;{props.person.name}
