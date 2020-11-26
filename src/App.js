2fdb9d72776751733d810122da58db43f40dff7c
chinna1574 <75009432+chinna1574@users.noreply.github.com>
2020-11-26 12:28:16 +0530

    hero component error handling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Person/Person';
+import Hero from './components/Person/Hero/Hero';
+
 
 function App() {
   return (
     <div className="App">
-      
-      <Person/>
+      <Hero heroName={'Batman'} />
+      <Hero heroName={'Joker'} />
     </div>
   );
 }
 
 export default App;
+
+
+/**
+ * {
+ *   heroName:undefined
+ * }
+ */
+
